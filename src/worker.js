e77d0391d3c02da64e40ab0bc6ebc933f7798a09
Arnon <arnonrdp@gmail.com>
2023-08-24 19:21:44 -0300

    Using pathnames as variable

--- src/worker.js
+++ src/worker.js
@@ -8,6 +8,9 @@ export default {
   async fetch(request, env, ctx) {
     const { pathname } = new URL(request.url)
 
+    const collectionName = pathname.split('/').filter(Boolean)[0]
+    const documentId = pathname.split('/').filter(Boolean)[1]
+
     switch (pathname) {
       case '/':
         return new Response(html, { headers: { 'content-type': 'text/html;charset=UTF-8' } })
@@ -15,12 +18,12 @@ export default {
       case '/company':
         return new Response(company, { headers: { 'Content-Type': 'application/json' } })
 
-      case '/prompts':
-        const prompts = await fetchFromFirestore('prompts')
-        return new Response(JSON.stringify(prompts, null, 2), { headers: { 'Content-Type': 'application/json' } })
+      case `/${collectionName}`:
+        const collection = await fetchFromFirestore(collectionName)
+        return new Response(JSON.stringify(collection, null, 2), { headers: { 'Content-Type': 'application/json' } })
 
-      case '/prompts/2023-07/visitors':
-        const visitors = await fetchFromFirestore('prompts/2023-07/visitors')
+      case `/${collectionName}/${documentId}/visitors`:
+        const visitors = await fetchFromFirestore(`${collectionName}/${documentId}/visitors`)
         return new Response(JSON.stringify(visitors, null, 2), { headers: { 'Content-Type': 'application/json' } })
 
       case '/v1':
