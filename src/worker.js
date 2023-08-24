9a4e12d67d90fb25290c15470492755ba6cb6299
Arnon <arnonrdp@gmail.com>
2023-08-24 14:37:22 -0300

    Created `/stats` route

--- src/worker.js
+++ src/worker.js
@@ -12,6 +12,9 @@ export default {
 
       case '/globe-and-citizen':
         return new Response(gc, { headers: { 'Content-Type': 'application/json' } })
+
+      case '/stats':
+        return new Response('Statistics will be placed here')
     }
 
     return new Response(page404, { headers: { 'content-type': 'text/html;charset=UTF-8' }, status: 404 })
