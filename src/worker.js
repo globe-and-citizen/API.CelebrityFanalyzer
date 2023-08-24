660c2dade6c3905cb5b32bf9b969539e03a285bb
Arnon <arnonrdp@gmail.com>
2023-08-24 14:23:55 -0300

    Started working with routes

--- src/worker.js
+++ src/worker.js
@@ -1,15 +1,16 @@
+import gc from './globe-and-citizen.js'
+import html from './index.html'
+
 export default {
   async fetch(request, env, ctx) {
-    const data = {
-      name: 'Globe & Citizen',
-      products: ['Celebrity Fanalyzer', 'Layer 8', 'Crypto Tips', 'Globe & Citizen']
-    }
-    const jsonData = JSON.stringify(data)
+    const { pathname } = new URL(request.url)
 
-    return new Response(jsonData, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
+    switch (pathname) {
+      case '/':
+        return new Response(html, { headers: { 'content-type': 'text/html;charset=UTF-8' } })
+
+      case '/globe-and-citizen':
+        return new Response(gc, { headers: { 'Content-Type': 'application/json' } })
+    }
   }
 }
