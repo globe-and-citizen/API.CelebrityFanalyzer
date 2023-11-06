2db0754b9a0a02826dea37125ab706d7ca7a059d
Arnon R <arnonrdp@gmail.com>
2023-11-06 11:07:13 -0300

    Added condition to verify param

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,13 @@
 const baseURL = 'https://firestore.googleapis.com/v1/projects/celebrityfanalyzer/databases/(default)/documents/'
 
-const fetchFromFirestore = async (path) => {
-  const response = await fetch(baseURL + path)
+const fetchFromFirestore = async (path, nextPageToken) => {
+  let url = baseURL + path
+
+  if (nextPageToken) {
+    url += '?pageToken=' + nextPageToken
+  }
+
+  const response = await fetch(url)
   return await response.json()
 }
 
