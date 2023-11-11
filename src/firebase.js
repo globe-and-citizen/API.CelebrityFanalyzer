db9fc140ad11392e0ae2ee1c924f63a728971a89
Arnon R <arnonrdp@gmail.com>
2023-11-11 07:37:33 -0300

    Refactor fetchFromFirestore to accept searchParams

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,14 @@
 const baseURL = 'https://firestore.googleapis.com/v1/projects/celebrityfanalyzer/databases/(default)/documents/'
 
-const fetchFromFirestore = async (path, nextPageToken) => {
+const fetchFromFirestore = async (path, searchParams) => {
   let url = baseURL + path
 
-  if (nextPageToken) {
-    url += '?pageToken=' + nextPageToken
+  if (searchParams.has('pageSize')) {
+    url += (url.includes('?') ? '&' : '?') + 'pageSize=' + searchParams.get('pageSize')
+  }
+
+  if (searchParams.has('pageToken')) {
+    url += (url.includes('?') || url.includes('&') ? '&' : '?') + 'pageToken=' + searchParams.get('pageToken')
   }
 
   const response = await fetch(url)
